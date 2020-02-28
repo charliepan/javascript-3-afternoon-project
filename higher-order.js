2b204fc921295b93f48f8a37fd88d3bb7b0ecc23
charliepan <hsinchih.pan@gmail.com>
2020-02-28 17:12:14 -0600

    Added code for higher order function

--- higher-order.js
+++ higher-order.js
@@ -21,7 +21,9 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 //Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+let evenNumbers = mixedNumbers.filter((val,i,arr) => {
+  return val % 2 === 0;
+});
 
 
 
@@ -44,8 +46,9 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
-
+let postTaxPrices  = prices.map((val,i,arr)=>{
+  return val * 1.07;
+});
 
 
 ////////// PROBLEM 3 //////////
@@ -63,7 +66,9 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+let totalPopulation  = populations.reduce((tot,val,i,arr)=>{
+  return tot + val;
+});
 
 
 
@@ -89,7 +94,9 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+let myStrongest = monstersInYourPocket.filter((val, i ,arr) => {
+  return val.CP > 200;
+});
 
 
 
@@ -106,8 +113,10 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
   Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate, hint: you'll need to do some multiplication). Your answer should be an array of numbers, one total for each order.
 */
 
-let orderTotals // Code here
-
+// Code here
+let orderTotals = orders.map((val, i, arr) => {
+  return (val.price * (val.tax + 1 ));
+});
 
 
 ////////// PROBLEM 6 //////////
@@ -126,6 +135,13 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
+let bobsTotal = purchases.reduce((tot,val,i,arr)=>{
+  if(val.owner === "Bob"){
+    return tot + val.price;
+  }
+  else{
+    return tot;
+  }
+},0);
 
 
